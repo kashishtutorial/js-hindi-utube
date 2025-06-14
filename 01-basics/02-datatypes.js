d6779a5ddac14926e2b1aa341790e9a3332e8ffc
kashishtutorial <batraanshita56@gmail.com>
2025-06-14 08:22:28 +0000

    datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as new version
+
+// alert(3+3)           //we are using nodejs, not browser
+
+console.log(3
+    +
+    3) //code readability should be high
+
+console.log("kashish")
+
+
+let name="kashish"
+let age= 17
+let isloggedin= false
+let state; 
+
+//NUMBER => 2 to power 53
+// bigint
+//String=> // ""
+//boolean=> true/false  
+//null => standlone value
+//undefined =>
+//symbols => uniue components
+
+
+//object
+
+ console.log(typeof undefined); //Undefined
+console.log(typeof  null); //object
