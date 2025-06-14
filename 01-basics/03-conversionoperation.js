df187a1bbbd9421f1466c76219e3816d829873a5
kashishtutorial <batraanshita56@gmail.com>
2025-06-14 13:53:06 +0000

    conversion of datatypes

---
+++ 01-basics/03-conversionoperation.js
@@ -0,0 +1,33 @@
+ let score= "kashish"
+
+ //console.log(typeof score);
+ // console.log(typeof (score));
+  
+   let valueinnumber= Number(score) 
+  // console.log(typeof valueinnumber);
+  // console.log( valueinnumber );
+
+  
+   //"33"=> 33
+   //"33abc"=> NaN{ NOT A NUMBER BUT TYPE IS NUMBER }
+   //TRUE=> 1; FALSE=> 0
+
+   let isloggedin= "kashish"
+
+   let bolleanisloggedin = Boolean(isloggedin)
+   //console.log(bolleanisloggedin );
+
+   //1 =>true; 0=> false
+   //""=> false 
+   //"kashish"=> true  
+
+   let someNumber = 33
+
+   let stringNumber = String(someNumber)
+   console.log(stringNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+   
