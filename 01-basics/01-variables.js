9a46723002d173b248b21418e0052e46b326948e
kashishtutorial <batraanshita56@gmail.com>
2025-06-14 08:25:55 +0000

    01-variables

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,21 @@
+const accountid=14453
+let accountemail="kashish@gmail.com"
+var accountpassword="12345"
+accountcity= "jaipur"
+let accountstate;
+
+// accountid= 2//not allowed
+
+
+accountemail="hc@hcgmail.com"
+accountpassword="21212121"
+accountcity="bengaluru"
+console.log(accountid);
+
+
+/* prefect not to use var 
+because of issue in block scope and functional scope
+ */
+
+console.table([ accountid,accountemail,accountpassword,accountcity,accountstate])
+
