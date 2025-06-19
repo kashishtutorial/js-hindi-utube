c0fea3f622874a4003e06d67327617c43d9dfef4
kashishtutorial <batraanshita56@gmail.com>
2025-06-19 04:19:44 +0000

    FUNCTIONS

---
+++ 03-basics/01-functions.js
@@ -0,0 +1,89 @@
+ 
+
+ function sayMyName(){
+   console.log("k");
+  console.log("a");
+ console.log("s");
+ console.log("h");
+ console.log("i");
+ console.log("s");
+ console.log("h");
+ }
+
+//  sayMyName()
+
+// function addTwoNumbers(number1,number2){
+//    console.log(number1+number2);
+// }
+
+function addTwoNumbers(number1,number2){
+// let result = number1+number2
+// console .log("kashish");
+
+return number1+number2
+return result
+
+}
+
+const result = addTwoNumbers(3,null)
+// console.log("Result:",result)
+
+
+function loginUserName(UserName = "sam"){
+   if(!UserName){
+      console.log("please enter a username")
+      return
+   }
+   return `${UserName} just looged in`
+}
+// console.log(loginUserName("Kashish"));
+// console.log(loginUserName());
+
+
+
+function calculateCartPrice(val1,val2,...num1){
+   return num1
+}
+// console.log(calculateCartPrice(200,300,500));
+
+const user = {
+username : "kashish",
+price : 199,
+}
+function handleObject(anyObject){
+   console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+   username : "sam",
+   price: 399
+})
+
+const myNewArray = [200,300,400]
+
+function returnsecondvalue(getArray){
+   return getArray[1]
+}
+
+// console.log(returnsecondvalue(myNewArray));
+console.log(returnsecondvalue([200,300,400]));
+
+
+
+
+
+
+
+
+// () in paranthesis we give value this is known as parameters
+//  and the value which is passes this is known as arguments...
+
+// whenever we return the value is saved in some variable...only console.log doesnt work''''.....
+//  !symbol convert false value into true aand true value into false  
+// and undefined is taken as false and empty string"" is taken as false value....
+
+
+
+
+
